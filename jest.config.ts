a79b228b32a3080024583da7241d9fceeae11d05
Stefan Kreitmayer <stefan.kreitmayer@babylonhealth.com>
2024-10-28 20:05:56 +0000

    Install jest and add example test

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'react-native',
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+    transformIgnorePatterns: [
+        'node_modules/(?!((jest-)?react-native|@react-native|@react-navigation))',
+    ],
+    transform: {
+        '^.+\\.[jt]sx?$': 'babel-jest',
+    },
+};
+
+export default config;
